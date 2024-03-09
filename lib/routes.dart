a79372b9caf05ec7aa3c15a0a23c998812044b84
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-03-09 22:04:10 +0530

    added new routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
 import 'screens/history_page.dart';
+import 'screens/trade_page.dart';
+import 'screens/about_page.dart';
+import 'screens/profile_page.dart';
 
 Map<String, WidgetBuilder> routes(BuildContext context) {
   return {
-    '/': (context) => HomePage(),
-    '/history': (context) => HistoryPage(),
+    '/': (context) => MyHomePage(),
+    '/history': (context) => HistoryPage(purchaseHistory: []),
+    '/trade': (context) => TradePage(),
+    '/about': (context) => AboutPage(),
+    '/profile': (context) => ProfilePage(),
   };
 }
