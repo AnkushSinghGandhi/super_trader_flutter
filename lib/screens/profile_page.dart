3dcd8c75a8b10dc503b2c66103ce9e0d851a8f0d
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-03-09 21:54:34 +0530

    profile page added

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,45 @@
+// profile_page.dart
+
+import 'package:flutter/material.dart';
+import '../widgets/drawer_widget.dart';
+
+class ProfilePage extends StatelessWidget {
+  final List<Map<String, dynamic>> options;
+
+  ProfilePage({required this.options});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 17, 19, 23),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Profile',
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: Icon(Icons.menu), // Menu icon for opening the drawer
+            onPressed: () {
+              Scaffold.of(context).openDrawer(); // Open the drawer using the context provided by the Builder
+            },
+          ),
+        ),
+      ),
+      drawer: DrawerWidget(
+        options: options,
+        onOptionSelected: (context, option) {
+          Scaffold.of(context).openDrawer(); // Open the drawer using the context provided by the Builder
+        },
+      ),
+      body: Center(
+        child: Text(
+          'This is the Profile Page',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
